af490ec2eb1f3a96c24f72f7ed94e4cdc7e7aaef
Ilias Papanikolaou <eliaspapanikolaou@hotmail.com>
2023-08-28 15:39:22 +0300

    Init commit

--- src/main/java/com/ipap/springboot3springsecurity6jwt/config/SecurityConfig.java
+++ src/main/java/com/ipap/springboot3springsecurity6jwt/config/SecurityConfig.java
@@ -1,12 +1,12 @@
 package com.ipap.springboot3springsecurity6jwt.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -19,23 +19,43 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
+    // Works with Spring Boot 3
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .csrf().disable()
+//                // Whitelist
+//                .authorizeHttpRequests().requestMatchers("/api/v1/auth/**").permitAll()
+//                // Any other request should be authenticated
+//                .anyRequest().authenticated()
+//                .and()
+//                // Stateless -> new session for each request
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                // Authentication Provider
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+//                .build();
+//
+//    }
+
+    // Works with Spring Boot 3.1
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable()
-                // Whitelist
-                .authorizeHttpRequests().requestMatchers("/api/v1/auth/**").permitAll()
-                // Any other request should be authenticated
-                .anyRequest().authenticated()
-                .and()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    // Whitelist
+                    auth.requestMatchers("/api/v1/auth/**").permitAll()
+                            // Any other request should be authenticated
+                            .anyRequest().authenticated();
+                })
                 // Stateless -> new session for each request
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // Authentication Provider
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-
     }
 }
